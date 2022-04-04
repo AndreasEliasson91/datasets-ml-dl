a16c02e39d4a7ed83538f31d932c7c1ecaada63e
AndreasEliasson91 <andreaseliasson91@gmail.com>
2022-04-04 10:43:59 +0200

    tested data and t-shirts sizes. Added Joakims solution

---
+++ body_measurement/joakim-solution/knn_predict.py
@@ -0,0 +1,66 @@
+import pandas as pd
+
+from math import sqrt
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def get_neighbours(k, height, weight, data_list):
+    dists = []
+    for point in data_list:
+        x = point[2]
+        y = point[1]
+        size = point[4]
+        dist = sqrt((weight-x)**2 + (height-y)**2)
+        dists.append({'dist': dist, 'size': size})
+
+    dists.sort(key=lambda d: d['dist'])
+    return dists[:k]
+
+
+def predict(neighbours):
+    sizes = {}
+    for dist_item in neighbours:
+        size = dist_item['size']
+        if size in sizes:
+            sizes[size] += 1
+        else:
+            sizes[size] = 1
+
+    k = len(neighbours)
+    return {size: value / k * 100 for size, value in sizes.items()}
+
+
+def main():
+    data_female = pd.read_csv('../data/cleaned_female.csv')
+    data_male = pd.read_csv('../data/cleaned_male.csv')
+
+    df = data_female.values.tolist() + data_male.values.tolist()
+
+    n = int(sqrt(len(df)))
+    k_values = [3, 5, 7, 9, 11, n]
+
+    height = int(input('Enter your height: '))
+    weight = int(input('Enter your weight: '))
+
+    for k in k_values:
+        neighbours = get_neighbours(k, height, weight, df)
+        result = predict(neighbours)
+        print(f'I predict that you should get a t-shirt size of (k={k})')
+        for size, proc in result.items():
+            print(f'\t{size} with a confidence of {proc :.2f}%')
+        print('-'*40)
+
+        heights = [data[1] for data in df]
+        weights = [data[2] for data in df]
+        labels = [data[4] for data in df]
+
+        data = list(zip(weights, heights))
+
+        knn_model = KNeighborsClassifier(n_neighbors=k)
+        knn_model.fit(data, labels)
+        print(f'sklearn predicted: {knn_model.predict([[weight, height]])} for k={k}')
+        print('-'*40)
+
+
+if __name__ == '__main__':
+    main()
---
+++ body_measurement/joakim-solution/prepare_data.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+class Person:
+    def __init__(self, age, height, weight, chest, size):
+        self.age = age
+        self.height = height
+        self.weight = weight
+        self.chest = chest
+        self.size = size
+
+    def __str__(self):
+        return f'{self.age},{self.height},{self.weight},{self.chest},{self.size}\n'
+
+
+def prepare_data(df, file_name):
+    ages = df.Age.tolist()
+    heights = [s/10 for s in df.stature.tolist()]
+    weights = [w/10 for w in df.weightkg.tolist()]
+    chests = [c/10 for c in df.chestcircumference.tolist()]
+
+    with open(file_name, 'w') as out_file:
+        out_file.write('Age,Height,Weight,Chest,Size\n')
+        for i in range(len(ages)):
+            chest = chests[i]
+
+            if chest < 84:
+                size = 'XXS'
+            elif chest < 90:
+                size = 'XS'
+            elif chest < 95:
+                size = 'S'
+            elif chest < 102:
+                size = 'M'
+            elif chest < 112:
+                size = 'L'
+            elif chest < 123:
+                size = 'XL'
+            elif chest < 133:
+                size = 'XXL'
+            else:
+                size = 'XXXL'
+
+            person = Person(ages[i], heights[i], weights[i], chest, size)
+            out_file.write(str(person))
+
+
+def main():
+    df = pd.read_csv('../data/female.csv')
+    prepare_data(df, '../data/cleaned_female.csv')
+    df = pd.read_csv('../data/male.csv')
+    prepare_data(df, '../data/cleaned_male.csv')
+
+
+if __name__ == '__main__':
+    main()
